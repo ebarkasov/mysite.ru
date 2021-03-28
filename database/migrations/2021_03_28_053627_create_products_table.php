88a75229380f1d44d7df4e919405b5d715bb5009
ebarkasov <75701391+ebarkasov@users.noreply.github.com>
2021-03-28 09:40:56 +0300

    update

--- database/migrations/2021_03_28_053627_create_products_table.php
+++ database/migrations/2021_03_28_053627_create_products_table.php
@@ -15,6 +15,10 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->integer('price');
+            $table->boolean('in_stock');
+            $table->text('description');
             $table->timestamps();
         });
     }
